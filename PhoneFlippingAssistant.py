2bd98b0cc3934425277e51d02a5acb1997f6c3fd
Montae <montae@Montaes-MacBook-Air.local>
2020-07-28 08:10:35 -0400

    added Cell positions for iPhone 7 unlocked 32 Gb

--- PhoneFlippingAssistant.py
+++ PhoneFlippingAssistant.py
@@ -1,6 +1,7 @@
-import openpyxl
-wb = openpyxl.load_workbook('PhoneFlippingGradingScale.xlsx') #calls the excel sheel where the prices are located
-sheet = wb.get_sheet_by_name('USED IPHONE') #calls the exact sheet in the excell sheet where the prices are located
+import openpyxl #imports the openpyxl module to be able to use and work with excel sheets
+path = 'PhoneFlippingGradingScale.xlsx' #this is the filename
+wb = openpyxl.load_workbook(path) #loads the excel sheet to be able to read from it
+usedIphoneSheet = wb.get_sheet_by_name('USED IPHONE') #links to the exact sheet
 
 #A class for the main border which iwll be used 
 class MenuBorder(): 
@@ -23,25 +24,49 @@ class GradingScale():
         print('\tD Grade: Fully functional, heavy dents/scratches or cracked, lcd lines./spots, no missing parts\n')
         for i in range(53): #specific border menu for the Grading Scale
             print(" *", end = "")
-#A class for confirmation to end program or continue 
-class Confirmation():
-    def confirmMsg(self):
-        confirmationMesage = input('\nWould you like to check another phone? Enter Y for yes or an N for no: ').upper()
 
+#This is a class that stores the cell positions for the phone prices 
+class CellPositions():
+   _iPhone7UL32GbAverage = usedIphoneSheet['C19'].value #Cell position for an average price of an 32gb unlocked iPhone
+   _iPhone7UL32GbAHigh = usedIphoneSheet['D19'].value #Cell position for A Grade High offer of an iPhone 7 unlocked 32Gb 
+   _iPhone7UL32GbALow = usedIphoneSheet['E19'].value #Cell position for A Grade Low offer of an iPhone 7 unlocked 32GB 
+   _iPhone7UL32GbBHigh = usedIphoneSheet['F9'].value #Cell position for B Grade High offer of an iPhone 7 unlocked 32GB 
+   _iPhone7UL32GbBLow = usedIphoneSheet['G19'].value #Cell position for B Grade Low offer of an iPhone 7 unlocked 32GB 
+   _iPhone7UL32GbCHigh = usedIphoneSheet['H19'].value #Cell position for C Grade High offer of an iPhone 7 unlocked 32GB
+   _iPhone7UL32GbCLow = usedIphoneSheet['I19'].value #Cell position for C Grade Low offer of an iPhone 7 unlocked 32GB
+   _iPhone7UL32GbDHigh = usedIphoneSheet['J19'].value #Cell position for D Grade High offer of an iPhone 7 unlocked 32GB 
+   _iPhone7UL32GbDLow = usedIphoneSheet['K19'].value #Cell position for D Grade Low offer of an iPhone 7 unlocked 32GB
+   
 
+   
+
+#Main Source Code 
+supportedPhones = {0: 'Grading Scale 📝', 1: 'iPhone 7 📱', 2: 'iPhone 7 Plus 📱', 3: 'iPhone 8 📱', 4: 'iPhone 8 Plus 📱', 5: 'iPhone X 📱', 6: 'iPhone XR 📱', 7: 'iPhone XS 📱', 8: 'iPhone XS Max 📱', 9: 'iPhone 11 📱', 10: 'iPhone 11 Pro 📱', 11: 'iPhone 11 Pro Max 📱'} #list of phones to choose from
 phoneOption = False #creates a variable for the following loop
 while phoneOption == False: #loop for the program to stay in so the user can continue or exit after checking on one phone
-    supportedPhones = {0: 'Grading Scale 📝', 1: 'iPhone 7 📱', 2: 'iPhone 7 Plus 📱', 3: 'iPhone 8 📱', 4: 'iPhone 8 Plus 📱', 5: 'iPhone X 📱', 6: 'iPhone XR 📱', 7: 'iPhone XS 📱', 8: 'iPhone XS Max 📱', 9: 'iPhone 11 📱', 10: 'iPhone 11 Pro 📱', 11: 'iPhone 11 Pro Max 📱'} #list of phones to choose from
     MenuBorder.border('*') #anytime you see this it is calling the Menu Border class to create a menu border 
     print('\t\t     🗳  Menu Options 🗳 :\n')
     for key, value in supportedPhones.items(): #Prints each items key and value in the supportedPhones Dict
         print('\t\t   ', key, ':', value)
     MenuBorder.border('*')
-    yourOption = eval(input('\n\nEnter the number of the phone you would like a price for: '))
-    if yourOption == 0:
+    phoneOption = True
+    yourOption = int(input('\n\nEnter the number of the phone you would like a price for: '))
+    if yourOption == 0: #Option for the Grading Scale 
         GradingScale.gradingRubric(print)
         print('\n\n')
-        if Confirmation.confirmMsg('self') == 'N':
-            print('Thanks for using this program')
+        #Confirmation message to either continue the program or quit it
+        confirmationMesage = input('\nWould you like to check another phone? Enter Y for yes or an N for no: ').upper()
+        if confirmationMesage == 'Y':
+            phoneOption = False
+        elif confirmationMesage == 'N':
+            print('\nThank you for using this program')
             phoneOption = True
+        else:
+            print('you entered an invalid charachter')
+            confirmationMesage = input('\nWould you like to check another phone? Enter Y for yes or an N for no: ').upper()
+    elif yourOption == 1:
+        print(usedIphoneSheet['D19'].value)
+
+
+
         
