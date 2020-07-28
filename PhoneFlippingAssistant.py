02806e46ed1c25a62c5acccbf45393c724dddb01
Montae <montae@Montaes-MacBook-Air.local>
2020-07-28 09:09:22 -0400

    Added iPhone 7+ average sale prices & offer prices

--- PhoneFlippingAssistant.py
+++ PhoneFlippingAssistant.py
@@ -27,13 +27,20 @@ class GradingScale():
 
 #This is a class that stores the average sale price of each phone
 class AverageSalePrice():
-       _iPhone7UL32GbAverage = usedIphoneSheet['C19'].value #Cell position for an average price of an 32gb unlocked iPhone 7
-       _iPhone7UL128GbAverage = usedIphoneSheet['C20'].value #Cell position for an average price of an 128gb unlocked iPhone 7
-       _iPhone7UL256GbAverage = usedIphoneSheet['C21'].value #Cell position for an average price of an 256gb unlocked iPhone 7
-       _iPhone7L32GbAverage = usedIphoneSheet['C22'].value #Cell position for an average price of an 32gb locked iPhone 7
-       _iPhone7L128GbAverage = usedIphoneSheet['C23'].value #Cell position for an average price of an 128gb locked iPhone 7
-       _iPhone7L256GbAverage = usedIphoneSheet['C24'].value #Cell position for an average price of an 256gb locked iPhone 7
-
+    #iPhone 7 Average Sale Prices
+    _iPhone7UL32GbAverage = usedIphoneSheet['C19'].value #Cell position for an average price of an 32gb unlocked iPhone 7
+    _iPhone7UL128GbAverage = usedIphoneSheet['C20'].value #Cell position for an average price of an 128gb unlocked iPhone 7
+    _iPhone7UL256GbAverage = usedIphoneSheet['C21'].value #Cell position for an average price of an 256gb unlocked iPhone 7
+    _iPhone7L32GbAverage = usedIphoneSheet['C22'].value #Cell position for an average price of an 32gb locked iPhone 7
+    _iPhone7L128GbAverage = usedIphoneSheet['C23'].value #Cell position for an average price of an 128gb locked iPhone 7
+    _iPhone7L256GbAverage = usedIphoneSheet['C24'].value #Cell position for an average price of an 256gb locked iPhone 7
+    #iPhone 7 Plus Average Sale Prices
+    _iPhone7PlusUL32GbAverage = usedIphoneSheet['C28'].value #Cell position for an average price of an 32gb unlocked iPhone 7 Plus
+    _iPhone7PlusUL128GbAverage = usedIphoneSheet['C29'].value #Cell position for an average price of an 128gb unlocked iPhone 7 Plus
+    _iPhone7PlusUL256GbAverage = usedIphoneSheet['C30'].value #Cell position for an average price of an 256gb unlocked iPhone 7 Plus
+    _iPhone7PlusL32GbAverage = usedIphoneSheet['C31'].value #Cell position for an average price of an 32gb locked iPhone 7 Plus
+    _iPhone7PlusL128GbAverage = usedIphoneSheet['C32'].value #Cell position for an average price of an 128gb locked iPhone 7 Plus
+    _iPhone7PlusL256GbAverage = usedIphoneSheet['C33'].value #Cell position for an average price of an 256gb locked iPhone 7 Plus
 
 #This is a class that stores the cell positions for the phone prices 
 class CellPositions():
@@ -63,7 +70,7 @@ class CellPositions():
    _iPhone7UL256GbDHigh = usedIphoneSheet['J21'].value #Cell position for D Grade High offer of an iPhone 7 unlocked 256Gb
    _iPhone7UL256GbDLow = usedIphoneSheet['K21'].value #Cell position for D Grade Low offer of an iPhone 7 unlocked 256Gb
    _iPhone7L32GbAHigh = usedIphoneSheet['D22'].value #Cell position for A Grade High offer of an iPhone 7 locked 32Gb 
-   _iPhone7L32GbALow = usedIphoneSheet['E122'].value #Cell position for A Grade Low offer of an iPhone 7 locked 32GB 
+   _iPhone7L32GbALow = usedIphoneSheet['E22'].value #Cell position for A Grade Low offer of an iPhone 7 locked 32GB 
    _iPhone7L32GbBHigh = usedIphoneSheet['F22'].value #Cell position for B Grade High offer of an iPhone 7 locked 32GB 
    _iPhone7L32GbBLow = usedIphoneSheet['G22'].value #Cell position for B Grade Low offer of an iPhone 7 locked 32GB 
    _iPhone7L32GbCHigh = usedIphoneSheet['H22'].value #Cell position for C Grade High offer of an iPhone 7 locked 32GB
@@ -86,14 +93,61 @@ class CellPositions():
    _iPhone7L256GbCLow= usedIphoneSheet['I24'].value #Cell position for C Grade Low offer of an iPhone 7 locked 256Gb
    _iPhone7L256GbDHigh = usedIphoneSheet['J24'].value #Cell position for D Grade High offer of an iPhone 7 locked 256Gb
    _iPhone7L256GbDLow = usedIphoneSheet['K24'].value #Cell position for D Grade Low offer of an iPhone 7 locked 256Gb
-    #Unlocked and Locked iPhone 7 Plus Prices
+   #Unlocked and Locked iPhone 7 Plus Prices
+   _iPhone7PlusUL32GbAHigh = usedIphoneSheet['D28'].value #Cell position for A Grade High offer of an iPhone 7 Plus unlocked 32Gb 
+   _iPhone7PlusUL32GbALow = usedIphoneSheet['E28'].value #Cell position for A Grade Low offer of an iPhone 7 Plus unlocked 32GB 
+   _iPhone7PlusUL32GbBHigh = usedIphoneSheet['28'].value #Cell position for B Grade High offer of an iPhone 7 Plus unlocked 32GB 
+   _iPhone7PlusUL32GbBLow = usedIphoneSheet['G28'].value #Cell position for B Grade Low offer of an iPhone 7 Plus unlocked 32GB 
+   _iPhone7PlusUL32GbCHigh = usedIphoneSheet['H28'].value #Cell position for C Grade High offer of an iPhone 7 Plus unlocked 32GB
+   _iPhone7PlusUL32GbCLow = usedIphoneSheet['I28'].value #Cell position for C Grade Low offer of an iPhone 7 Plus unlocked 32GB
+   _iPhone7PlusUL32GbDHigh = usedIphoneSheet['J28'].value #Cell position for D Grade High offer of an iPhone 7 Plus unlocked 32GB 
+   _iPhone7PlusUL32GbDLow = usedIphoneSheet['K28'].value #Cell position for D Grade Low offer of an iPhone 7 Plus unlocked 32GB
+   _iPhone7PlusUL128GbAHigh = usedIphoneSheet['D29'].value #Cell position for A Grade High offer of an iPhone 7 Plus unlocked 128Gb
+   _iPhone7PlusUL128GbALow = usedIphoneSheet['E29'].value #Cell position for A Grade Low offer of an iPhone 7 Plus unlocked 128Gb
+   _iPhone7PlusUL128GbBHigh = usedIphoneSheet['F29'].value #Cell position for B Grade High offer of an iPhone 7 Plus unlocked 128Gb
+   _iPhone7PlusUL128GbBLow = usedIphoneSheet['G29'].value #Cell position for B Grade Low offer of an iPhone 7 Plus unlocked 128Gb
+   _iPhone7PlusUL128GbCHigh = usedIphoneSheet['H29'].value #Cell position for C Grade High offer of an iPhone 7 Plus unlocked 128Gb
+   _iPhone7PlusUL128GbCLow = usedIphoneSheet['I29'].value #Cell position for C Grade Low offer of an iPhone 7 Plus unlocked 128Gb
+   _iPhone7PlusUL128GbDHigh = usedIphoneSheet['J29'].value #Cell position for D Grade High offer of an iPhone 7 Plus unlocked 128Gb
+   _iPhone7PlusUL128GbDlow = usedIphoneSheet['K29'].value #Cell position for D Grade Low offer of an iPhone 7 Plus unlocked 128Gb
+   _iPhone7PlusUL256GbAHigh = usedIphoneSheet['D30'].value #Cell position for A Grade High offer of an iPhone 7 Plus unlocked 256Gb
+   _iPhone7PlusUL256GbALow = usedIphoneSheet['E30'].value #Cell position for A Grade Low offer of an iPhone 7 Plus unlocked 256Gb
+   _iPhone7PlusUL256GbBHigh = usedIphoneSheet['F30'].value #Cell position for B Grade High offer of an iPhone 7 Plus unlocked 256Gb
+   _iPhone7PlusUL256GbBLow = usedIphoneSheet['G30'].value #Cell position for B Grade Low offer of an iPhone 7 Plus unlocked 256Gb
+   _iPhone7PlusUL256GbCHigh = usedIphoneSheet['H30'].value #Cell position for C Grade High offer of an iPhone 7 Plus unlocked 256Gb
+   _iPhone7PlusUL256GbCLow= usedIphoneSheet['I30'].value #Cell position for C Grade Low offer of an iPhone 7 Plus unlocked 256Gb
+   _iPhone7PlusUL256GbDHigh = usedIphoneSheet['J30'].value #Cell position for D Grade High offer of an iPhone 7 Plus unlocked 256Gb
+   _iPhone7PlusUL256GbDLow = usedIphoneSheet['K30'].value #Cell position for D Grade Low offer of an iPhone 7 Plus unlocked 256Gb
+   _iPhone7PlusL32GbAHigh = usedIphoneSheet['D31'].value #Cell position for A Grade High offer of an iPhone 7 Plus locked 32Gb 
+   _iPhone7PlusL32GbALow = usedIphoneSheet['E31'].value #Cell position for A Grade Low offer of an iPhone 7 Plus locked 32GB 
+   _iPhone7PlusL32GbBHigh = usedIphoneSheet['F31'].value #Cell position for B Grade High offer of an iPhone 7 Plus locked 32GB 
+   _iPhone7PlusL32GbBLow = usedIphoneSheet['G31'].value #Cell position for B Grade Low offer of an iPhone 7 Plus locked 32GB 
+   _iPhone7PlusL32GbCHigh = usedIphoneSheet['H31'].value #Cell position for C Grade High offer of an iPhone 7 Plus locked 32GB
+   _iPhone7PlusL32GbCLow = usedIphoneSheet['I31'].value #Cell position for C Grade Low offer of an iPhone 7 Plus locked 32GB
+   _iPhone7PlusL32GbDHigh = usedIphoneSheet['J31'].value #Cell position for D Grade High offer of an iPhone 7 Plus locked 32GB 
+   _iPhone7PlusL32GbDLow = usedIphoneSheet['K31'].value #Cell position for D Grade Low offer of an iPhone 7 Plus locked 32GB
+   _iPhone7PlusL128GbAHigh = usedIphoneSheet['D32'].value #Cell position for A Grade High offer of an iPhone 7 Plus locked 128Gb
+   _iPhone7PlusL128GbALow = usedIphoneSheet['E32'].value #Cell position for A Grade Low offer of an iPhone 7 Plus locked 128Gb
+   _iPhone7PlusL128GbBHigh = usedIphoneSheet['F32'].value #Cell position for B Grade High offer of an iPhone 7 Plus locked 128Gb
+   _iPhone7PlusL128GbBLow = usedIphoneSheet['G32'].value #Cell position for B Grade Low offer of an iPhone 7 Plus locked 128Gb
+   _iPhone7PlusL128GbCHigh = usedIphoneSheet['H32'].value #Cell position for C Grade High offer of an iPhone 7 Plus locked 128Gb
+   _iPhone7PlusL128GbCLow = usedIphoneSheet['I32'].value #Cell position for C Grade Low offer of an iPhone 7 Plus locked 128Gb
+   _iPhone7PlusL128GbDHigh = usedIphoneSheet['J32'].value #Cell position for D Grade High offer of an iPhone 7 Plus locked 128Gb
+   _iPhone7PlusL128GbDlow = usedIphoneSheet['K32'].value #Cell position for D Grade Low offer of an iPhone 7 Plus locked 128Gb
+   _iPhone7PlusL256GbAHigh = usedIphoneSheet['D33'].value #Cell position for A Grade High offer of an iPhone 7 Plus locked 256Gb
+   _iPhone7PlusL256GbALow = usedIphoneSheet['E33'].value #Cell position for A Grade Low offer of an iPhone 7 Plus locked 256Gb
+   _iPhone7PlusL256GbBHigh = usedIphoneSheet['F33'].value #Cell position for B Grade High offer of an iPhone 7 Plus locked 256Gb
+   _iPhone7PlusL256GbBLow = usedIphoneSheet['G33'].value #Cell position for B Grade Low offer of an iPhone 7 Plus locked 256Gb
+   _iPhone7PlusL256GbCHigh = usedIphoneSheet['H33'].value #Cell position for C Grade High offer of an iPhone 7 Plus locked 256Gb
+   _iPhone7PlusL256GbCLow= usedIphoneSheet['I33'].value #Cell position for C Grade Low offer of an iPhone 7 Plus locked 256Gb
+   _iPhone7PlusL256GbDHigh = usedIphoneSheet['J33'].value #Cell position for D Grade High offer of an iPhone 7 Plus locked 256Gb
+   _iPhone7PlusL256GbDLow = usedIphoneSheet['K33'].value #Cell position for D Grade Low offer of an iPhone 7 Plus locked 256Gb
 
 
 
 
 
 
-    
 #Main Source Code 
 supportedPhones = {0: 'Grading Scale 📝', 1: 'iPhone 7 📱', 2: 'iPhone 7 Plus 📱', 3: 'iPhone 8 📱', 4: 'iPhone 8 Plus 📱', 5: 'iPhone X 📱', 6: 'iPhone XR 📱', 7: 'iPhone XS 📱', 8: 'iPhone XS Max 📱', 9: 'iPhone 11 📱', 10: 'iPhone 11 Pro 📱', 11: 'iPhone 11 Pro Max 📱'} #list of phones to choose from
 phoneOption = False #creates a variable for the following loop
